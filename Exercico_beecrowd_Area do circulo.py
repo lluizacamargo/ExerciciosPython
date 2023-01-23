1638bc6da48cbf8011b7d2c307e803019d57a0c2
Luciana <116033232+lluizacamargo@users.noreply.github.com>
2023-01-23 17:55:09 -0300

    Área Circulo

---
+++ Exercico_beecrowd_Area do circulo.py
@@ -0,0 +1,9 @@
+# A fórmula para calcular a área de uma circunferência é: area = π . raio2. Considerando para este problema que π = 3.14159:
+#Efetue o cálculo da área, elevando o valor de raio ao quadrado e multiplicando por n.
+
+
+raio = float(input())
+area = round(3.14159*raio**2, 4)
+area = '{:6.4f}'.format(area)
+print(f'A={area}')
+
