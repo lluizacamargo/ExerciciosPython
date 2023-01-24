5abb7ab7506fd3fb9f9974d2ddfedf41f0fd98ae
Luciana <116033232+lluizacamargo@users.noreply.github.com>
2023-01-24 15:35:57 -0300

    uri-problem-solution-in-python
    Problema 1008 Salary

---
+++ Exercico_beecrowd_Salario.py
@@ -0,0 +1,9 @@
+
+numfunc = int(input())
+numhoras = int(input())
+valorhora = round(float(input()),2)
+
+salario = numhoras * valorhora
+
+print('NUMBER = ', numfunc)
+print('SALARY = ', 'U$', round(salario,2))
