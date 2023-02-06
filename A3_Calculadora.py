7041cb8f75085f79f652ef666b75e2461a48425c
Luciana <116033232+lluizacamargo@users.noreply.github.com>
2023-02-06 16:29:46 -0300

    Calculadora_Python

---
+++ A3_Calculadora.py
@@ -0,0 +1,28 @@
+print('CALCULADORA')
+print(' + Adição')
+print(' - Subtração')
+print(' * Multiplicação')
+print(' / Divisão')
+print('Precisone outra tecla para Sair')
+
+op = input ('Qual operação deseja fazer? ')
+x = int(input('Digite o primeiro valor: '))
+y = int(input ('Digite o segundo valor: '))
+
+if (op == '+'):
+    res = x + y
+    print('Resultado: {} + {} = {}'.format(x, y, res))
+elif (op == '-'):
+    res = x - y
+    print('Resultado: {} - {} = {}'.format(x, y, res))
+elif (op == '*'):
+    res = x * y
+    print('Resultado: {} * {} = {}'.format(x, y, res))
+elif (op == '/'):
+    res = x / y
+    print('Resultado: {} / {} = {}'.format(x, y, res))
+
+else: #Vai entrar nessa condição para o caso do usuario não escolher nenhuma opção acima.
+    print('Operação invalida!')
+
+print('Encerrando o programa...')
